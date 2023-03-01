a0c4e53a0418645f1745438a87a2d56faa505c6b
Laert Xhumari <laertxhumari@gmail.com>
2023-03-01 18:06:37 -0500

    created questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,61 @@
-// TODO: Include packages needed for this application
+//DEPENDENCIES =============================================================================
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+//DATA =====================================================================================
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the project name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter a DESCRIPTION line or enter nothing to continue to the next section",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter a INSTALLATION INSTRUCTION line or enter nothing to continue to the next section",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Enter a USAGE INSTRUCTION line or enter nothing to continue to the next section",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter a CONTRIBUTION GUIDELINE or enter nothing to continue to the next section",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Enter a TEST INSTRUCTION or enter nothing to continue to the next section",
+        name: "test"
+    },
+    
 
+];
+
+
+//FUNCTIONS ================================================================================
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
 
+//USER INTERACTIONS ========================================================================
+
+//INITIALIZATIONS =========================================================================
 // Function call to initialize app
 init();
+
+
+
+
+
+
+
